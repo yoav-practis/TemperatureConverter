72e103ede909b89c97156d63efb091b586cde01d
Yoav Levenson <yoav@practis.co.il>
2022-11-07 13:00:48 +0000

    Add project files.

---
+++ TemperatureConverter/Celcius.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter
+{
+    public class Celcius
+    {
+        Logger logger = new Logger();
+
+        public double CelciusToFahrenheit(double celcius)
+        {
+            double result = (celcius * 1.8) + 32;
+            logger.Log($"{celcius} in Celcius are {result} in Fahrenheit");
+            return result;
+        }
+
+        public double FahrenheitToCelcius(double fahrenheit)
+        {
+            double result = (fahrenheit - 32) / 1.8;
+            logger.Log($"{fahrenheit} in Fahrenheit are {result} in Celcius");
+            return result;
+        }
+    }
+}
---
+++ TemperatureConverter/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter
+{
+    public class Logger
+    {
+        public bool Log(string message)
+        {
+            string fullMessage = EnhanceMessage(message);
+            Console.WriteLine(fullMessage);
+            return true;
+        }
+
+        private string EnhanceMessage(string message)
+        {
+            Random random = new Random();
+            int chanceOfFailing = random.Next(0, 2);
+
+            if (chanceOfFailing > 0)
+            {
+                throw new Exception("Oh no! Something went wrong!");
+            }
+            else
+            {
+                return "[INFO] " + message;
+            }
+        }
+    }
+}
